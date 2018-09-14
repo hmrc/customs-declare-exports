5ddbaca9423ab56101949d3e4617a338d48fb9f1
Patryk Rudnicki <patrudnicki@gmail.com>
2018-09-14 13:48:54 +0100

    [CEDS-601] Configuration and tests

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,9 +14,10 @@ object AppDependencies {
   def test(scope: String = "test") = Seq(
     "uk.gov.hmrc" %% "hmrctest" % "3.1.0" % scope,
     "org.scalatest" %% "scalatest" % "3.0.4" % scope,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % "test",
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
 
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+    "org.mockito" % "mockito-core" % "2.13.0" % "test"
   )
-
 }
