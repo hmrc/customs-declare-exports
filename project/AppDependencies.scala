06ac78cdb5aad53970f55aa9e4a403427fd1c375
Patryk Rudnicki <patrudnicki@gmail.com>
2019-01-07 09:29:15 +0000

    Upgrade logger version - related with changes play version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,8 @@ object AppDependencies {
     "uk.gov.hmrc" %% "play-reactivemongo" % "6.2.0",
     ws,
     "uk.gov.hmrc" %% "bootstrap-play-25" % "3.8.0",
-    "uk.gov.hmrc" %% "wco-dec" % "0.16.0"
+    "uk.gov.hmrc" %% "wco-dec" % "0.16.0",
+    "uk.gov.hmrc" %% "logback-json-logger" % "4.2.0"
   )
 
   def test(scope: String = "test") = Seq(
@@ -16,7 +17,6 @@ object AppDependencies {
     "org.scalatest" %% "scalatest" % "3.0.4" % scope,
     "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % "test",
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
-
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
     "org.mockito" % "mockito-core" % "2.13.0" % "test"
   )
