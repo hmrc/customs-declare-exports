a619dc9dbc72f68e5b6a3c3a6352e3507bbd9578
Patryk Rudnicki <23506899+PatrykRudnicki@users.noreply.github.com>
2019-05-01 10:10:50 +0100

    CEDS-1140 Upgrade for libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,19 +5,19 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "play-reactivemongo" % "6.2.0",
+    "uk.gov.hmrc" %% "play-reactivemongo" % "6.7.0",
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.6.0",
-    "uk.gov.hmrc" %% "wco-dec" % "0.23.0",
-    "uk.gov.hmrc" %% "logback-json-logger" % "4.1.0"
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.11.0",
+    "uk.gov.hmrc" %% "wco-dec" % "0.30.0",
+    "uk.gov.hmrc" %% "logback-json-logger" % "4.6.0"
   )
 
   def test(scope: String = "test") = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "3.1.0" % scope,
-    "org.scalatest" %% "scalatest" % "3.0.4" % scope,
+    "uk.gov.hmrc" %% "hmrctest" % "3.8.0-play-25" % scope,
+    "org.scalatest" %% "scalatest" % "3.0.5" % scope,
     "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % "test",
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-    "org.mockito" % "mockito-core" % "2.13.0" % "test"
+    "org.mockito" % "mockito-core" % "2.27.0" % "test"
   )
 }
