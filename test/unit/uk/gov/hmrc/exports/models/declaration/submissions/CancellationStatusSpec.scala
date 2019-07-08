9c6e55d6944130579de7ea724eff2d8622588ae7
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2019-07-08 16:12:03 +0100

    CEDS-942 Move Cancellation and Submission Status Specs
    Both suites are moved to dedicated package.

--- test/unit/uk/gov/hmrc/exports/models/declaration/submissions/CancellationStatusSpec.scala
+++ test/unit/uk/gov/hmrc/exports/models/declaration/submissions/CancellationStatusSpec.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package unit.uk.gov.hmrc.exports.models.declaration
+package unit.uk.gov.hmrc.exports.models.declaration.submissions
 
 import org.scalatest.{MustMatchers, WordSpec}
 import play.api.libs.json._
