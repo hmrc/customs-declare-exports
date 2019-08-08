1527cadaf797d35faa38beb26cd4941166c8486f
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-08-08 11:25:40 +0100

    CEDS-1479 Change date type to Instant

--- app/uk/gov/hmrc/exports/models/declaration/ExportsDeclaration.scala
+++ app/uk/gov/hmrc/exports/models/declaration/ExportsDeclaration.scala
@@ -1,9 +1,9 @@
 package uk.gov.hmrc.exports.models.declaration
 
-import java.time.{Instant, LocalDateTime, ZoneOffset}
+import java.time.Instant
 import java.util.UUID
 
-import play.api.libs.json.{JsError, JsNumber, JsObject, JsResult, JsSuccess, JsValue, Json, OFormat}
+import play.api.libs.json._
 
 case class ExportsDeclaration
 (
@@ -12,15 +12,15 @@ case class ExportsDeclaration
 )
 
 object ExportsDeclaration {
-  implicit val formatInstant: OFormat[LocalDateTime] = new OFormat[LocalDateTime] {
-    override def writes(datetime: LocalDateTime): JsObject =
-      Json.obj("$date" -> datetime.toInstant(ZoneOffset.UTC).toEpochMilli)
+  implicit val formatInstant: OFormat[Instant] = new OFormat[Instant] {
+    override def writes(datetime: Instant): JsObject =
+      Json.obj("$date" -> datetime.toEpochMilli)
 
-    override def reads(json: JsValue): JsResult[LocalDateTime] =
+    override def reads(json: JsValue): JsResult[Instant] =
       json match {
         case JsObject(map) if map.contains("$date") =>
           map("$date") match {
-            case JsNumber(v) => JsSuccess(Instant.ofEpochMilli(v.toLong).atOffset(ZoneOffset.UTC).toLocalDateTime)
+            case JsNumber(v) => JsSuccess(Instant.ofEpochMilli(v.toLong))
             case _           => JsError("Unexpected Date Format. Expected a Number (Epoch Milliseconds)")
           }
         case _ => JsError("Unexpected Date Format. Expected an object containing a $date field.")
