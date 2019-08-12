0c1e82823333785192d66e5be76d64e2ad5fbeea
Paulo Monteiro <paulo.from.portugal@gmail.com>
2019-08-12 16:25:17 +0100

    CEDS-1480 Add a country verification service
    This is to map the country ISO 2 codes into a country name.

---
+++ test/uk/gov/hmrc/exports/services/CountriesServiceSpec.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.exports.services
+
+import org.scalatestplus.play.PlaySpec
+import uk.gov.hmrc.exports.models.Country
+
+class CountriesServiceSpec extends PlaySpec {
+
+  "Countries" should {
+
+    "give all countries with codes in alphabetical order of country name" in {
+      val threeCountries = CountriesService.allCountries.filter(
+        c => c.countryName == "Afghanistan" || c.countryName == "Mayotte" || c.countryName == "Zimbabwe"
+      )
+      threeCountries mustBe List(Country("Afghanistan", "AF"), Country("Mayotte", "YT"), Country("Zimbabwe", "ZW"))
+    }
+  }
+}
