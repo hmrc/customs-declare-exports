d5143535205a4e1756eb770a92fc387071bf512a
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-10-09 09:28:18 +0100

    CEDS-1695 Slight service alteration as not all pointers will match

--- app/uk/gov/hmrc/exports/services/WCOPointerMappingService.scala
+++ app/uk/gov/hmrc/exports/services/WCOPointerMappingService.scala
@@ -3,10 +3,21 @@ package uk.gov.hmrc.exports.services
 import javax.inject.Inject
 import uk.gov.hmrc.exports.models.Pointer
 
+import scala.io.Source
+
 class WCOPointerMappingService @Inject()(){
 
-  def mapWCOPointerToExportsPointer(pointer: Pointer): Pointer = ???
+  private lazy val mappings: Set[(String, String)] = {
+    val source = Source.fromFile("/code-lists/pointer-mappings.csv")
+    try {
+      source.getLines().map(_.split(",")).map {
+        case Array(wcoPointer, exportsPointer) => (wcoPointer, exportsPointer)
+      }.toSet
+    } finally source.close()
+  }
 
-  def mapWCOPointerToExportsPointer(pointers: Seq[Pointer]): Seq[Pointer] = pointers.map(mapWCOPointerToExportsPointer)
+  def mapWCOPointerToExportsPointer(pointer: Pointer): Option[Pointer] = ???
+
+  def mapWCOPointerToExportsPointer(pointers: Seq[Pointer]): Seq[Pointer] = pointers.map(mapWCOPointerToExportsPointer).filter(_.isDefined).flatten
 
 }
