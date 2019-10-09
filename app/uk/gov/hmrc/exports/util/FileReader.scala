4142408d20328accfea5e14b5fc252108f81c8d4
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-10-09 12:25:46 +0100

    CEDS-1695 File Reader Spec

--- app/uk/gov/hmrc/exports/util/FileReader.scala
+++ app/uk/gov/hmrc/exports/util/FileReader.scala
@@ -7,11 +7,17 @@ import scala.io.Source
 @Singleton
 class FileReader @Inject()(){
 
-  def readLines(filename: String): List[String] = {
+  def readLines(filename: String, skipHeaderLine: Boolean = false): List[String] = {
+    val lines = read(filename)
+      .filter(_.nonEmpty) // Ignore empty lines
+      .filter(!_.startsWith("#")) // Ignore comment lines
+    if(skipHeaderLine) lines.tail else lines
+  }
+
+  private def read(filename: String): List[String] = {
     val source = Source.fromFile(filename)
     try {
-      source
-        .getLines().toList
+      source.getLines().toList
     } finally source.close()
   }
 
---
+++ test/uk/gov/hmrc/exports/util/FileReaderSpec.scala
@@ -0,0 +1,19 @@
+package uk.gov.hmrc.exports.util
+
+import org.scalatest.{MustMatchers, WordSpec}
+
+class FileReaderSpec extends WordSpec with MustMatchers {
+
+  private val reader = new FileReader()
+
+  "Read lines" should {
+    "not skip header by default" in {
+      reader.readLines("/header-file.csv").head mustBe "header-line"
+    }
+
+    "skip header" in {
+      reader.readLines("/header-file.csv", skipHeaderLine = true).head mustBe "data"
+    }
+  }
+
+}
