9c4e559c0417cb554ca8ade544e0405299e5d102
pmonteiro <paulo.from.portugal@gmail.com>
2020-02-06 21:12:48 +0000

    CEDS-2111 Pretty format dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,20 +7,20 @@ object AppDependencies {
   private val testScope = "test, it, component"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.22.0-play-26",
+    "uk.gov.hmrc"             %%  "simple-reactivemongo"  % "7.22.0-play-26",
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.3.0",
-    "uk.gov.hmrc" %% "wco-dec" % "0.33.0",
-    "uk.gov.hmrc" %% "logback-json-logger" % "4.6.0",
-    "com.typesafe.play" %% "play-json-joda" % "2.6.10"
+    "uk.gov.hmrc"             %%  "bootstrap-play-26"     % "1.3.0",
+    "uk.gov.hmrc"             %%  "wco-dec"               % "0.33.0",
+    "uk.gov.hmrc"             %%  "logback-json-logger"   % "4.6.0",
+    "com.typesafe.play"       %%  "play-json-joda"        % "2.6.10"
   )
 
   def test(scope: String = "test") = Seq(
-    "org.scalatest" %% "scalatest" % "3.0.8" % testScope,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % testScope,
-    "com.github.tomakehurst" % "wiremock-jre8" % "2.25.1" % testScope,
-    "org.pegdown" % "pegdown" % "1.6.0" % testScope,
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % testScope,
-    "org.mockito" % "mockito-core" % "3.0.0" % "test"
+    "org.scalatest"           %% "scalatest"            % "3.0.8"             % testScope,
+    "org.scalatestplus.play"  %% "scalatestplus-play"   % "3.1.2"             % testScope,
+    "com.github.tomakehurst"  % "wiremock-jre8"         % "2.25.1"            % testScope,
+    "org.pegdown"             % "pegdown"               % "1.6.0"             % testScope,
+    "com.typesafe.play"       %% "play-test"            % PlayVersion.current % testScope,
+    "org.mockito"             % "mockito-core"          % "3.0.0"             % "test"
   )
 }
