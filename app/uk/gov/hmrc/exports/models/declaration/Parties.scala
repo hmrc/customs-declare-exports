d0d2d7a7d21a28126ec435d7f87d5a9162c4a71d
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2020-05-05 10:06:51 +0100

    CEDS-2401 - Ask if Declarant is the Exporter (#188)

--- app/uk/gov/hmrc/exports/models/declaration/Parties.scala
+++ app/uk/gov/hmrc/exports/models/declaration/Parties.scala
@@ -43,6 +43,13 @@ object DeclarantDetails {
   implicit val format: OFormat[DeclarantDetails] = Json.format[DeclarantDetails]
 }
 
+case class DeclarantIsExporter(answer: String) {
+  def isExporter: Boolean = answer == "Yes"
+}
+object DeclarantIsExporter {
+  implicit val format: OFormat[DeclarantIsExporter] = Json.format[DeclarantIsExporter]
+}
+
 case class RepresentativeDetails(details: Option[EntityDetails], statusCode: Option[String], representingOtherAgent: Option[String])
 object RepresentativeDetails {
   implicit val format: OFormat[RepresentativeDetails] = Json.format[RepresentativeDetails]
@@ -80,6 +87,7 @@ case class Parties(
   exporterDetails: Option[ExporterDetails] = None,
   consigneeDetails: Option[ConsigneeDetails] = None,
   declarantDetails: Option[DeclarantDetails] = None,
+  declarantIsExporter: Option[DeclarantIsExporter] = None,
   representativeDetails: Option[RepresentativeDetails] = None,
   declarationAdditionalActorsData: Option[DeclarationAdditionalActors] = None,
   declarationHoldersData: Option[DeclarationHolders] = None,
