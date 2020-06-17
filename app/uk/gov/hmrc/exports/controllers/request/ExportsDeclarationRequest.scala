32c8e7f3f2a19e3d55ea144b72a2861d2baab0af
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2020-06-17 15:27:57 +0100

    CEDS-2469 - changed new status to CANDIDATE and added test

--- app/uk/gov/hmrc/exports/controllers/request/ExportsDeclarationRequest.scala
+++ app/uk/gov/hmrc/exports/controllers/request/ExportsDeclarationRequest.scala
@@ -43,7 +43,7 @@ case class ExportsDeclarationRequest(
   def toExportsDeclaration(id: String, eori: Eori): ExportsDeclaration = ExportsDeclaration(
     id = id,
     eori = eori.value,
-    status = consignmentReferences.map(_ => DeclarationStatus.DRAFT).getOrElse(DeclarationStatus.INITIAL),
+    status = consignmentReferences.map(_ => DeclarationStatus.DRAFT).getOrElse(DeclarationStatus.CANDIDATE),
     createdDateTime = this.createdDateTime,
     updatedDateTime = this.updatedDateTime,
     sourceId = this.sourceId,
