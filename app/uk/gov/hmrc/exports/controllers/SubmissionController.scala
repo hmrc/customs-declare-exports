afb902f2b2f754ed5de0f5ad590d1af2f76e6ea7
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2020-11-25 17:33:54 +0000

    [WIP] Simplify declaration submission process

--- app/uk/gov/hmrc/exports/controllers/SubmissionController.scala
+++ app/uk/gov/hmrc/exports/controllers/SubmissionController.scala
@@ -16,31 +16,34 @@
 
 package uk.gov.hmrc.exports.controllers
 
-import scala.concurrent.{ExecutionContext, Future}
-
-import javax.inject.{Inject, Singleton}
 import play.api.mvc._
 import uk.gov.hmrc.exports.controllers.actions.Authenticator
 import uk.gov.hmrc.exports.controllers.response.ErrorResponse
-import uk.gov.hmrc.exports.services.{DeclarationService, SubmissionService}
+import uk.gov.hmrc.exports.repositories.DeclarationRepository
+import uk.gov.hmrc.exports.services.SubmissionService
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class SubmissionController @Inject()(
   authenticator: Authenticator,
   submissionService: SubmissionService,
-  declarationService: DeclarationService,
-  cc: ControllerComponents
+  cc: ControllerComponents,
+  declarationRepository: DeclarationRepository
 )(implicit executionContext: ExecutionContext)
     extends RESTController(cc) with JSONResponses {
 
   def create(id: String): Action[Unit] = authenticator.authorisedAction(parse.empty) { implicit request =>
-    declarationService.findOne(id, request.eori).flatMap {
-      case Some(declaration) =>
-        if (declaration.isCompleted) {
+    declarationRepository.markCompleted(id, request.eori).flatMap {
+
+      case Some(declarationBeforeUpdate) =>
+        if (declarationBeforeUpdate.isCompleted) {
           Future.successful(Conflict(ErrorResponse("Declaration has already been submitted")))
         } else {
-          submissionService.submit(declaration).map(Created(_))
+          submissionService.submit(declarationBeforeUpdate).map(Created(_))
         }
+
       case None => Future.successful(NotFound)
     }
   }
