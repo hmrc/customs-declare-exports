22a6590252cbc02b7c2b6ed2003b83f101b72e93
Tim <glidester@gmail.com>
2021-06-16 11:31:31 +0100

    [CEDS-3267] Play 2.8 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,23 +6,24 @@ object AppDependencies {
   private val testScope = "test, it"
 
   val compile = Seq(
-    "uk.gov.hmrc"                    %%  "simple-reactivemongo"         % "7.31.0-play-27",
-    "uk.gov.hmrc"                    %%  "bootstrap-backend-play-27"    % "3.3.0",
+    "uk.gov.hmrc"                    %%  "simple-reactivemongo"         % "8.0.0-play-28",
+    "uk.gov.hmrc"                    %%  "bootstrap-backend-play-28"    % "5.3.0",
     "uk.gov.hmrc"                    %%  "wco-dec"                      % "0.35.0",
-    "uk.gov.hmrc"                    %%  "logback-json-logger"          % "4.8.0",
+    "uk.gov.hmrc"                    %%  "logback-json-logger"          % "5.1.0",
     "com.github.cloudyrock.mongock"  %   "mongock-core"                 % "2.0.2",
     "org.mongodb.scala"              %%  "mongo-scala-driver"           % "2.9.0",
-    "com.github.tototoshi"           %%  "scala-csv"                    % "1.3.6",
-    "uk.gov.hmrc"                    %%  "work-item-repo"               % "7.11.0-play-27"
+    "com.github.tototoshi"           %%  "scala-csv"                    % "1.3.8",
+    "uk.gov.hmrc"                    %%  "work-item-repo"               % "8.0.0-play-28",
+    "com.fasterxml.jackson.module"   %%  "jackson-module-scala"         % "2.12.3"
   )
 
   val test = Seq(
-    "org.scalatest"          %% "scalatest"                % "3.2.3"             % testScope,
-    "org.scalatest"          %% "scalatest-featurespec"    % "3.2.3"             % testScope,
+    "org.scalatest"          %% "scalatest"                % "3.2.9"             % testScope,
+    "org.scalatest"          %% "scalatest-featurespec"    % "3.2.9"             % testScope,
     "com.typesafe.play"      %% "play-test"                % PlayVersion.current % testScope,
-    "org.scalatestplus.play" %% "scalatestplus-play"       % "4.0.3"             % testScope,
+    "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"             % testScope,
     "com.vladsch.flexmark"   %  "flexmark-all"             % "0.36.8"            % testScope,
-    "com.github.tomakehurst" %  "wiremock-jre8"            % "2.27.2"            % testScope,
-    "org.mockito"            %% "mockito-scala"            % "1.16.32"           % "test"
+    "com.github.tomakehurst" %  "wiremock-jre8"            % "2.28.1"            % testScope,
+    "org.mockito"            %% "mockito-scala"            % "1.16.37"           % "test"
   )
 }
