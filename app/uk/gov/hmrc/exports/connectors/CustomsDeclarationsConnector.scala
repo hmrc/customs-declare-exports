9e7463551c6855b68a41b9574508fd449710b87b
Tom Robinson <25112739+magicaljellybeans@users.noreply.github.com>
2023-03-02 15:38:04 +0000

    CEDS-4551 Change logger statement level to debug

--- app/uk/gov/hmrc/exports/connectors/CustomsDeclarationsConnector.scala
+++ app/uk/gov/hmrc/exports/connectors/CustomsDeclarationsConnector.scala
@@ -121,7 +121,7 @@ class CustomsDeclarationsConnector @Inject() (appConfig: AppConfig, httpClient: 
               reportAs = Status.INTERNAL_SERVER_ERROR
             )
           case _ =>
-            logger.warn(s"Received status:${response.status} with headers: ${response.headers}")
+            logger.debug(s"Received status:${response.status} with headers: ${response.headers}")
             CustomsDeclarationsResponse(
               response.status,
               Some(
