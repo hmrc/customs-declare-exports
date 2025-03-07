7e5682af098fdafaa54c12a1cd3be6c780edb91a
Ian Wood <86672642+IanWood-HMRC@users.noreply.github.com>
2025-03-07 16:43:18 +0000

    DCWL-2930 made vals private as the apply() method should be used.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,22 +5,22 @@ object Dependencies {
   val bootstrapPlayVersion = "9.10.0"
   val hmrcMongoVersion = "2.5.0"
 
-  val compile: Seq[ModuleID] = List(
+  private val compile = List(
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30"          % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-work-item-repo-play-30"  % hmrcMongoVersion,
     "uk.gov.hmrc"            %% "wco-dec"                            % "0.40.0",
-    "commons-codec"          %  "commons-codec"                      % "1.17.1",
+    "commons-codec"          %  "commons-codec"                      % "1.18.0",
     "org.glassfish.jaxb"     %  "jaxb-runtime"                       % "4.0.5",
   )
 
-  val test: Seq[ModuleID] = List(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-30"             % bootstrapPlayVersion % "test",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"            % hmrcMongoVersion     % "test",
-    "com.vladsch.flexmark"   %  "flexmark-all"                       % "0.64.8"             % "test",
-    "org.mockito"            %% "mockito-scala-scalatest"            % "1.17.37"            % "test",
-    "org.scalatest"          %% "scalatest"                          % "3.2.19"             % "test",
-    "org.scalatestplus"      %% "scalacheck-1-15"                    % "3.2.11.0"           % "test"
-  )
+  private val test = List(
+    "uk.gov.hmrc"            %% "bootstrap-test-play-30"             % bootstrapPlayVersion,
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"            % hmrcMongoVersion,
+    "com.vladsch.flexmark"   %  "flexmark-all"                       % "0.64.8",
+    "org.mockito"            %% "mockito-scala-scalatest"            % "1.17.37",
+    "org.scalatest"          %% "scalatest"                          % "3.2.19",
+    "org.scalatestplus"      %% "scalacheck-1-15"                    % "3.2.11.0"
+  ).map(_ % Test)
 
   def apply(): Seq[ModuleID] =
     (compile ++ test).map(moduleId => if (moduleId.name == "flexmark-all") moduleId else moduleId.withSources)
