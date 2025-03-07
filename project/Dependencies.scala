ad3c0abde3373e90eb65aedb091fd84598a12895
Ian Wood <86672642+IanWood-HMRC@users.noreply.github.com>
2025-03-07 16:44:14 +0000

    DCWL-2930 added val for flexmark

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,9 @@ import sbt._
 
 object Dependencies {
 
-  val bootstrapPlayVersion = "9.10.0"
-  val hmrcMongoVersion = "2.5.0"
+  private val bootstrapPlayVersion = "9.10.0"
+  private val hmrcMongoVersion = "2.5.0"
+  private val flexmark = "flexmark-all"
 
   private val compile = List(
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30"          % bootstrapPlayVersion,
@@ -16,12 +17,12 @@ object Dependencies {
   private val test = List(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"             % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"            % hmrcMongoVersion,
-    "com.vladsch.flexmark"   %  "flexmark-all"                       % "0.64.8",
+    "com.vladsch.flexmark"   %  flexmark                       % "0.64.8",
     "org.mockito"            %% "mockito-scala-scalatest"            % "1.17.37",
     "org.scalatest"          %% "scalatest"                          % "3.2.19",
     "org.scalatestplus"      %% "scalacheck-1-15"                    % "3.2.11.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] =
-    (compile ++ test).map(moduleId => if (moduleId.name == "flexmark-all") moduleId else moduleId.withSources)
+    (compile ++ test).map(moduleId => if (moduleId.name == flexmark) moduleId else moduleId.withSources)
 }
