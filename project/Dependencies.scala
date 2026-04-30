0e4652e892c9ac03f513bff6747e7b72c084b594
AdamCoops <adam.cooper2@digital.hmrc.gov.uk>
2026-04-30 13:56:52 +0100

    DCWL-4438-update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,8 +10,8 @@ object Dependencies {
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30"          % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-work-item-repo-play-30"  % hmrcMongoVersion,
     "uk.gov.hmrc"            %% "wco-dec"                            % "0.43.0",
-    "commons-codec"          %  "commons-codec"                      % "1.22.0",
-    "org.glassfish.jaxb"     %  "jaxb-runtime"                       % "4.0.7",
+    "commons-codec"          %  "commons-codec"                      % "1.20.0",
+    "org.glassfish.jaxb"     %  "jaxb-runtime"                       % "4.0.6",
   )
 
   private val test = List(
@@ -19,7 +19,7 @@ object Dependencies {
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"            % hmrcMongoVersion,
     "com.vladsch.flexmark"   %  flexmark                             % "0.64.8",
     "org.scalatestplus"      %% "mockito-4-11"                       % "3.2.18.0",
-    "org.scalatest"          %% "scalatest"                          % "3.2.20",
+    "org.scalatest"          %% "scalatest"                          % "3.2.19",
     "org.scalatestplus"      %% "scalacheck-1-15"                    % "3.2.11.0"
   ).map(_ % Test)
 
